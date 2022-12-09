8e059642cce9251f2dbfcf73edf8e4c9a7791ff4
Jordan Butterfield <jmcbutter@gmail.com>
2022-12-09 12:46:20 -0600

    Remove unused Code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import GameScreen from "./components/4-pages/GameScreen";
 import StartingScreen from "./components/4-pages/StartingScreen";
-import Player from "./utils/Player";
-import Game from "./utils/Game";
 
 function App() {
-  const player1 = Player(1, "YOU", "x", false);
-  const player2 = Player(2, "CPU", "o", true);
-
-  const newGame = Game();
-  newGame.setPlayers([player1, player2]);
-  newGame.startNewGame();
-
-  const [game, setGame] = useState(newGame);
+  const [game, setGame] = useState();
 
   return (
     <div className="App">
-      {/* <GameScreen game={game} setGame={setGame} /> */}
       {game ? (
         <GameScreen game={game} setGame={setGame} />
       ) : (
