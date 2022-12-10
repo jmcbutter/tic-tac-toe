be220c0894d774b569eef068ba72b1cc9e89ac90
Jordan Butterfield <jmcbutter@gmail.com>
2022-12-09 18:59:42 -0600

    Try to build minimax function

--- src/utils/Player.js
+++ src/utils/Player.js
@@ -31,6 +31,35 @@ export default function Player(id, name, icon, isCPU) {
     return emptyIndices[0];
   }
 
+  function minimax(boardCopy, emptyIndices, maximizingPlayer) {
+    const winningSquares = boardCopy.getWinningSquares();
+
+    if (winningSquares[0].getIcon() == icon) return { score: 10 };
+    else if (winningSquares.length > 0) return { score: -10 };
+    else if (emptyIndices.length == 0) return { score: 0 };
+
+    emptyIndices.forEach((i) => {
+      let move = {};
+      move.index = boardCopy.getSquares()[emptyIndices[i]];
+
+      boardCopy.makeSelection({ position: i, player: maximizingPlayer });
+    });
+
+    if (maximizingPlayer) {
+      let maxEv = -Infinity;
+      emptyIndices.forEach((index) => {
+        const ev = minimax(boardCopy, emptyIndices, false);
+        maxEv = max(maxEv, ev);
+      });
+    } else {
+      let minEv = -Infinity;
+      emptyIndices.forEach((index) => {
+        const ev = minimax(boardCopy, emptyIndices, true);
+        minEv = min(minEv, ev);
+      });
+    }
+  }
+
   return {
     isCPU,
     getId,
